f0143a44e22b4028011e51a94d78c1ca9098d55f
Rushikesh <rushikesh.s.warde@gmail.com>
2024-08-23 19:21:13 +0530

    1.Added screenshots for all projects. 2. Updated captions for all screenshots in their respective json files. 3. Removed unnecessary images.

--- src/assets/imageImports.js
+++ src/assets/imageImports.js
@@ -2,17 +2,20 @@
 import vsg1 from "assets/img/projects/vsg-1.png";
 import vsg2 from "assets/img/projects/vsg-2.png";
 import vsg3 from "assets/img/projects/vsg-3.png";
-import vsg4 from "assets/img/projects/vsg-4.png";
 import vsg5 from "assets/img/projects/vsg-5.png";
 import vsg6 from "assets/img/projects/vsg-6.png";
 
-import aau1 from "assets/img/projects/aau-1.jpg";
-import aau2 from "assets/img/projects/aau-2.jpg";
-import aau3 from "assets/img/projects/aau-3.jpg";
+import aau1 from "assets/img/projects/aau-1.png";
+import aau2 from "assets/img/projects/aau-2.png";
+import aau3 from "assets/img/projects/aau-3.png";
+import aau4 from "assets/img/projects/aau-4.png";
+import aau5 from "assets/img/projects/aau-5.png";
 
-import fps1 from "assets/img/projects/fps-1.jpg";
-import fps2 from "assets/img/projects/fps-2.jpg";
-import fps3 from "assets/img/projects/fps-3.jpg";
+import fps1 from "assets/img/projects/fps-1.png";
+import fps2 from "assets/img/projects/fps-2.png";
+import fps3 from "assets/img/projects/fps-3.png";
+import fps4 from "assets/img/projects/fps-4.png";
+import fps5 from "assets/img/projects/fps-5.png";
 
 import fl1 from "assets/img/projects/fl-1.png";
 import fl2 from "assets/img/projects/fl-2.png";
@@ -21,45 +24,64 @@ import fl4 from "assets/img/projects/fl-4.png";
 import fl5 from "assets/img/projects/fl-5.png";
 import fl6 from "assets/img/projects/fl-6.png";
 
-import jed1 from "assets/img/projects/jed-1.jpg";
-import jed2 from "assets/img/projects/jed-2.jpg";
-import jed3 from "assets/img/projects/jed-3.jpg";
+import jed1 from "assets/img/projects/jed-1.png";
+import jed2 from "assets/img/projects/jed-2.png";
+import jed3 from "assets/img/projects/jed-3.png";
+import jed4 from "assets/img/projects/jed-4.png";
 
 import mg1 from "assets/img/projects/mg-1.png";
 import mg2 from "assets/img/projects/mg-2.png";
 import mg3 from "assets/img/projects/mg-3.png";
+import mg4 from "assets/img/projects/mg-4.png";
 
-import imt1 from "assets/img/projects/imt-1.jpg";
-import imt2 from "assets/img/projects/imt-2.jpg";
-import imt3 from "assets/img/projects/imt-3.jpg";
+import imt1 from "assets/img/projects/imt-1.png";
+import imt2 from "assets/img/projects/imt-2.png";
+import imt3 from "assets/img/projects/imt-3.png";
+import imt4 from "assets/img/projects/imt-4.png";
+import imt5 from "assets/img/projects/imt-5.png";
+import imt6 from "assets/img/projects/imt-6.png";
+import imt7 from "assets/img/projects/imt-7.png";
+import imt8 from "assets/img/projects/imt-8.png";
 
-import ta1 from "assets/img/projects/ta-1.jpg";
-import ta2 from "assets/img/projects/ta-2.jpg";
-import ta3 from "assets/img/projects/ta-3.jpg";
+import ta1 from "assets/img/projects/ta-1.png";
+import ta2 from "assets/img/projects/ta-2.png";
+import ta3 from "assets/img/projects/ta-3.png";
+import ta4 from "assets/img/projects/ta-4.png";
 
-import idv1 from "assets/img/projects/idv-1.jpg";
-import idv2 from "assets/img/projects/idv-2.jpg";
-import idv3 from "assets/img/projects/idv-3.jpg";
+import idv1 from "assets/img/projects/idv-1.png";
+import idv2 from "assets/img/projects/idv-2.png";
+import idv3 from "assets/img/projects/idv-3.png";
+import idv4 from "assets/img/projects/idv-4.png";
+import idv5 from "assets/img/projects/idv-5.png";
 
-import rp1 from "assets/img/projects/rp-1.jpg";
-import rp2 from "assets/img/projects/rp-2.jpg";
-import rp3 from "assets/img/projects/rp-3.jpg";
+import rp1 from "assets/img/projects/rp-1.png";
+import rp2 from "assets/img/projects/rp-2.png";
+import rp3 from "assets/img/projects/rp-3.png";
+import rp4 from "assets/img/projects/rp-4.png";
+import rp5 from "assets/img/projects/rp-5.png";
+import rp6 from "assets/img/projects/rp-6.png";
+import rp7 from "assets/img/projects/rp-7.png";
+import rp8 from "assets/img/projects/rp-8.png";
+import rp9 from "assets/img/projects/rp-9.png";
 
-import cs from "assets/img/coming-soon.jpg"
+import cs from "assets/img/coming-soon.png"
 
 export const images = {
   vsg1,
   vsg2,
   vsg3,
-  vsg4,
   vsg5,
   vsg6,
   aau1,
   aau2,
   aau3,
+  aau4,
+  aau5,
   fps1,
   fps2,
   fps3,
+  fps4,
+  fps5,
   fl1,
   fl2,
   fl3,
@@ -69,20 +91,36 @@ export const images = {
   jed1,
   jed2,
   jed3,
+  jed4,
   mg1,
   mg2,
   mg3,
+  mg4,
   imt1,
   imt2,
   imt3,
+  imt4,
+  imt5,
+  imt6,
+  imt7,
+  imt8,
   ta1,
   ta2,
   ta3,
+  ta4,
   idv1,
   idv2,
   idv3,
+  idv4,
+  idv5,
   rp1,
   rp2,
   rp3,
+  rp4,
+  rp5,
+  rp6,
+  rp7,
+  rp8,
+  rp9,
   cs
 };

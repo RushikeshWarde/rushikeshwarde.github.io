ec24bab2d320e48d860262842a34b1f746c6a33f
Rushikesh <rushikesh.s.warde@gmail.com>
2024-08-24 00:12:18 +0530

    1. Updated indsex.html with correct data. 2. 2. Added a preview image. 3. Changed BrowserRouter to HashRouter to deal with 404 in github-pages.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import "assets/css/bootstrap.min.css";
 import "assets/scss/now-ui-kit.scss?v=1.5.0";
@@ -22,7 +22,7 @@ import ARProjectData from "data/ARProjects.json";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/tabs" element={<Tabs />} />
       <Route path="/about" element={<ProfilePage />} />
@@ -49,5 +49,5 @@ root.render(
         }
       />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );

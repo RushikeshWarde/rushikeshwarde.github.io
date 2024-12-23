a89fc5f3e1580f8cdda95fe88352c1c9e8573da1
Rushikesh <rushikesh.s.warde@gmail.com>
2024-12-23 14:57:09 +0530

    1. ProfilePage.js - Made the VR Projects button to pulse so that it attracts the attension of the clients. 2. custom-components.css - Added the css for button pulse and change background to darker color on hover

--- src/views/ProfilePage.js
+++ src/views/ProfilePage.js
@@ -1,13 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"; 
 import { Link } from "react-router-dom";
-import {
-  Button,
-  TabContent,
-  TabPane,
-  Container,
-  Row,
-  Col
-} from "reactstrap";
+import { Button, TabContent, TabPane, Container, Row, Col } from "reactstrap";
 import Navbar from "components/Navbars/ProfileNavbar";
 import ProfilePageHeader from "components/Headers/ProfilePageHeader.js";
 import DefaultFooter from "components/Footers/Footer.js";
@@ -65,7 +58,7 @@ function ProfilePage() {
                           className="img-raised first-tab collections-custom"
                           src={require(`assets/img/portfolio/${item.image}`)}
                         ></img>
-                        <p className="carousel-caption-profile d-none d-md-block"><strong class="caption-text-profile">{item.caption}</strong></p>
+                        <p className="carousel-caption-profile d-none d-md-block"><strong className="caption-text-profile">{item.caption}</strong></p>
                       </Col>
                     ))}
                   </Row>
@@ -74,25 +67,32 @@ function ProfilePage() {
               </TabContent>
             </Row>
             <Row>
-              {data.projectButtons.map((project, index) => (
-                <Col className="ml-auto mr-auto" md="4" xl="4" key={index}>
-                  <div className="nav-align-center">
-                    <Link to={project.link}>
-                      <Button className="btn btn-info btn-lg project-button" size="lg">
-                        <p><h6 style={{
-                        marginTop: "1px",
-                        marginBottom: "1px"
-                    }}><img src={icons[project.icon]}
-                            style={{ width: "20px", height: "20px" }}
-                            alt=""
-                          />{" "}
-                          {project.title}
-                          </h6></p>
-                      </Button>
-                    </Link>
-                  </div>
-                </Col>
-              ))}
+              {data.projectButtons.map((project, index) => {
+                // Conditionally add 'pulsing-button' and 'full-width-button' classes
+                const isVrProject = project.title === "VR PROJECTS";
+                const buttonClasses = `btn btn-lg ${isVrProject ? 'full-width-button pulsing-button' : 'btn-info project-button'}`;
+                
+                return (
+                  <Col className="ml-auto mr-auto" md="4" xl="4" key={index}>
+                    <div className="nav-align-center">
+                      <Link to={project.link}>
+                        <Button className={buttonClasses} size="lg">
+                          <p>
+                            <h6 style={{ marginTop: "1px", marginBottom: "1px" }}>
+                              <img 
+                                src={icons[project.icon]} 
+                                style={{ width: "20px", height: "20px" }} 
+                                alt="" 
+                              />{" "}
+                              {project.title}
+                            </h6>
+                          </p>
+                        </Button>
+                      </Link>
+                    </div>
+                  </Col>
+                );
+              })}
             </Row>
           </Container>
         </div>
